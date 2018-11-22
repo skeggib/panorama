a03abd027b7d41a8b0da0e9d98c94c73818e408d
Sebastien Klasa <klasa.sebastien@gmail.com>
2018-11-22 12:39:32 +0100

    Find the transformation matrix and create the panorama

--- headers/panorama.hpp
+++ headers/panorama.hpp
@@ -4,6 +4,7 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 #include <array>
+#include <cstdlib>
 #include "panorama.hpp"
 #include "fast_detector.hpp"
 #include "local_maxima.hpp"
@@ -50,11 +51,11 @@ std::vector<corner<CornerSize>> corners_detector(const cv::Mat & image) {
 }
 
 template<int CornerSize>
-std::vector<std::pair<corner<CornerSize>, corner<CornerSize>>> pair_corners(
+std::vector<std::pair<cv::Point, cv::Point>> pair_corners(
     std::vector<corner<CornerSize>> corners1,
     std::vector<corner<CornerSize>> corners2
 ) {
-    std::vector<std::pair<corner<9>, corner<9>>> pairs;
+    std::vector<std::pair<cv::Point, cv::Point>> pairs;
     for (auto corner1 : corners1) {
         double bestScore = std::numeric_limits<double>::infinity();
         corner<9> bestCorner;
@@ -72,9 +73,98 @@ std::vector<std::pair<corner<CornerSize>, corner<CornerSize>>> pair_corners(
                 bestCorner = corner2;
             }
         }
-        pairs.push_back({ corner1, bestCorner });
+        pairs.push_back({ corner1.position, bestCorner.position });
     }
     return pairs;
+}
+
+cv::Point transform(cv::Point p, cv::Mat t) {
+    cv::Mat m(3, 1, CV_32F);
+    m.at<float>(0, 0) = (float)p.x;
+    m.at<float>(1, 0) = (float)p.y;
+    m.at<float>(2, 0) = 1;
+    cv::Mat r = t * m;
+    r /= r.at<float>(2, 0);
+    cv::Point pp;
+    pp.x = (int)r.at<float>(0, 0);
+    pp.y = (int)r.at<float>(1, 0);
+    return pp;
+}
+
+cv::Mat find_transform(std::vector<std::pair<cv::Point, cv::Point>> pairs, std::vector<std::pair<cv::Point, cv::Point>>& selectedPairs) {
+    std::srand((uint)std::time(nullptr));
+
+    int bestScore = 0;
+    cv::Mat bestTransform(3, 3, CV_32F);
+    for (int iter = 0; iter < 1000; iter++) {
+        std::array<int, 5> indexes;
+        for (int i = 0; i < indexes.size(); i++) {
+            indexes[i] = std::rand() % pairs.size();
+            bool exists = false;
+            for (int j = 0; j < i && !exists; j++)
+                if (indexes[i] == indexes[j])
+                    exists = true;
+            if (exists)
+                i--;
+        }
+
+        selectedPairs.clear();
+        for (int i = 0; i < indexes.size(); i++)
+            selectedPairs.push_back(pairs[indexes[i]]);
+
+        cv::Mat A(10, 9, CV_32F);
+        for (int i = 0; i < selectedPairs.size(); i++) {
+            const auto& pair = selectedPairs[i];
+            A.at<float>(i*2, 0) = (float)pair.first.x;
+            A.at<float>(i*2, 1) = (float)pair.first.y;
+            A.at<float>(i*2, 2) = 1.;
+            for (int j = 3; j < 6; j++)
+                A.at<float>(i*2, j) = 0.;
+            A.at<float>(i*2, 6) = (float)-pair.second.x * pair.first.x;
+            A.at<float>(i*2, 7) = (float)-pair.second.x * pair.first.y;
+            A.at<float>(i*2, 8) = (float)-pair.second.x;
+            
+            for (int j = 0; j < 3; j++)
+                A.at<float>(i*2+1, j) = 0.;
+            A.at<float>(i*2+1, 3) = (float)pair.first.x;
+            A.at<float>(i*2+1, 4) = (float)pair.first.y;
+            A.at<float>(i*2+1, 5) = 1.;
+            A.at<float>(i*2+1, 6) = (float)-pair.second.y * pair.first.x;
+            A.at<float>(i*2+1, 7) = (float)-pair.second.y * pair.first.y;
+            A.at<float>(i*2+1, 8) = (float)-pair.second.y;
+        }
+        cv::Mat b(9, 1, CV_32F);
+        for (int i = 0; i < b.size().height - 1; i++)
+            b.at<float>(i, 0) = 0.;
+        b.at<float>(b.size().height - 1, 0) = 1.;
+        cv::Mat x;
+        cv::solve(A, b, x, cv::DECOMP_SVD);
+        
+        cv::Mat transformMat(3, 3, CV_32F);
+        auto* pTransformMat = transformMat.data;
+        auto* pX = x.data;
+        for (int i = 0; i < x.size().height * sizeof(float); i++, pTransformMat++, pX++)
+            *pTransformMat = *pX;
+
+        int score = 0;
+        for (auto pair : pairs) {
+            auto im = transform(pair.first, transformMat);
+            double xd = im.x - pair.second.x;
+            double yd = im.y - pair.second.y;
+            if (xd*xd + yd*yd < 10)
+                score++;
+        }
+        if (score >= bestScore) {
+            std::cout << score << std::endl;
+            bestScore = score;
+            auto* pBestTransform = bestTransform.data;
+            pX = x.data;
+            for (int i = 0; i < x.size().height * sizeof(float); i++, pBestTransform++, pX++)
+                *pBestTransform = *pX;
+        }
+    }
+    
+    return bestTransform;
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -32,14 +32,44 @@ int main(int argc, char** argv) {
     // Find corners in images
     auto corners1 = corners_detector<9>(image1);
     auto corners2 = corners_detector<9>(image2);
-
     auto detectionTime = std::clock();
     std::cout << "Corners detection: " << (detectionTime - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
 
     // Find pair of corners
     auto pairs = pair_corners<9>(corners1, corners2);
+    auto pairingTime = std::clock();
+    std::cout << "Corners pairing: " << (pairingTime - detectionTime) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
 
-    std::cout << "Corners pairing: " << (std::clock() - detectionTime) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
+    // Find transformation
+    std::vector<std::pair<cv::Point, cv::Point>> selectedPairs;
+    auto transformMat = find_transform(pairs, selectedPairs);
+    auto findTransformTime = std::clock();
+    std::cout << "Finding transform: " << (findTransformTime - pairingTime) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
+
+    auto p00 = transform(cv::Point(0, 0), transformMat);
+    auto p10 = transform(cv::Point(image1.size().width - 1, 0), transformMat);
+    auto p01 = transform(cv::Point(0, image1.size().height - 1), transformMat);
+    auto p11 = transform(cv::Point(image1.size().width - 1, image1.size().height - 1), transformMat);
+
+    auto minX = std::min<int>({ p00.x, p10.x, p01.x, p11.x, 0 });
+    auto maxX = std::max<int>({ p00.x, p10.x, p01.x, p11.x, image2.size().width - 1 });
+    auto minY = std::min<int>({ p00.y, p10.y, p01.y, p11.y, 0 });
+    auto maxY = std::max<int>({ p00.y, p10.y, p01.y, p11.y, image2.size().height - 1 });
+    
+    cv::Mat inverseTransformMat;
+    cv::invert(transformMat, inverseTransformMat);
+    cv::Mat panoramaImage(cv::Size(maxX - minX + 1, maxY - minY + 1), CV_8UC1, UCHAR_MAX);
+    image2.copyTo(panoramaImage(cv::Rect(-minX, -minY, image2.size().width, image2.size().height)));
+    auto* pPanorama = panoramaImage.data;
+    for (int i = 0; i < panoramaImage.size().height; i++) {
+        for (int j = 0; j < panoramaImage.size().width; j++, pPanorama++) {
+            cv::Point p(j + minX, i + minY);
+            auto pp = transform(p, inverseTransformMat);
+            if (pp.x >= 0 && pp.x < image1.size().width - 1 &&
+                pp.y >= 0 && pp.y < image1.size().height - 1)
+                *pPanorama = (uchar)(((int)image1.at<uchar>(pp) + (int)*pPanorama) / 2);
+        }
+    }
     
     // Draw rectangles around corners in image 1
     for (const auto& corner : corners1) {
@@ -62,14 +92,14 @@ int main(int argc, char** argv) {
     image2.copyTo(roiImage2);
 
     // Draws lines for pairs of corners
-    for (auto pair : pairs) {
-        auto p1 = pair.first.position;
-        auto p2 = pair.second.position;
+    for (auto pair : selectedPairs) {
+        auto p1 = pair.first;
+        auto p2 = pair.second;
         p2.x += image1.size().width;
         cv::line(images, p1, p2, UCHAR_MAX);
     }
 
-    cv::imshow("corners", images);
+    cv::imshow("corners", panoramaImage);
     cv::waitKey();
 
     return 0;
